55a922d8262be7735dbad2a81e4bae145b71d23e
ADITYAchhipa <142408817+ADITYAchhipa@users.noreply.github.com>
2026-03-19 12:49:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+let app = express();
+
+
+app.get("/",(req,res)=>{
+    res.json({message:"hello from universe"});
+})
+
+
+app.get("/health",(req,res)=>{
+    res.json({message:"Route is Healthty"});
+})
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
